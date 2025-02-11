8d24dfd9820ce2d35471f676b4ff91b1e977a3eb
lcs-muniz <lucas.muniz.ifpr@gmail.com>
2025-02-11 13:16:20 -0300

    (feat): main app

--- vscode/src/main/java/com/fazenda/App.java
+++ vscode/src/main/java/com/fazenda/App.java
@@ -1,1 +1,13 @@
 package com.fazenda;
+
+import com.fazenda.service.FazendeiroService;
+import com.fazenda.view.GameView;
+
+public class App {
+
+    public static void main(String[] args) {
+        FazendeiroService fazendeiroService = new FazendeiroService();
+        GameView gameView = new GameView(fazendeiroService);
+        gameView.iniciarJogo();
+    }
+}
